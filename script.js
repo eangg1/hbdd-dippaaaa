ff07164f109da5bc317172ef9cec7317790bf82b
eangg1 <mazraihanz@gmail.com>
2025-03-13 15:48:21 +0700

    commit

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ navigator.mediaDevices.getUserMedia({ audio: true })
             const average = sum / bufferLength;
 
             // Sesuaikan threshold agar tidak langsung mati saat halaman dimuat
-            if (average > 128) { 
+            if (average > 115) { 
                 if (!isBlowing && !isFlameOff) {
                     isBlowing = true;
                     toggleFlame(); // Mematikan api
